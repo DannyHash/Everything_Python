7b6285971386f87a098754673c98f57edca45655
Hash <dannyhash0x1@gmail.com>
2025-04-04 23:49:58 -0400

    Added initial implementation of Python syntax and indentation exercise

---
+++ Python_Learning_And_Cheatsheets/Python_Syntax_Indentation_Variables_And_Types/exercise1.py
@@ -0,0 +1,27 @@
+"""
+Practical Exercise 1: Python Syntax & Indentation
+Task:
+Write a Python program that does the following:
+
+Declares a variable name and assigns it your name.
+
+Declares a variable age and assigns it your age.
+
+Uses proper indentation to check if age is greater than or equal to 18:
+
+If age >= 18, print "You are an adult."
+
+Otherwise, print "You are a minor."
+"""
+
+name = "Danny"
+age = 23
+
+print(f"Hello, my name is {name}")
+
+if age >= 18:
+    print("I am an adult.")
+else:
+    print("I am a minor.")
+
+# The program declares a variable name and assigns it your name.
