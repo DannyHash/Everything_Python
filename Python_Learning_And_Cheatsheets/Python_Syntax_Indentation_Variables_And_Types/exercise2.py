2edabfb9fc23c2fe857344e227974aa0c9913c83
Hash <dannyhash0x1@gmail.com>
2025-04-05 00:23:38 -0400

    Added exercise for declaring variables and performing data type operations

---
+++ Python_Learning_And_Cheatsheets/Python_Syntax_Indentation_Variables_And_Types/exercise2.py
@@ -0,0 +1,34 @@
+"""
+Practical Exercise 2: Variables & Data Types
+Task:
+Declare three variables:
+
+num1 (integer)
+
+num2 (float)
+
+text (string)
+
+Perform the following operations:
+
+Add num1 and num2 and store the result in sum_result.
+
+Convert sum_result into an integer and store it in sum_int.
+
+Print all variables with their types using type().
+"""
+
+num1 = 10
+num2 = 20.5
+text = "Hackers rock!"
+
+sum_result = num1 + num2
+sum_int = int(sum_result)
+
+print(f"num1: {num1}, type: {type(num1)}")
+print(f"num2: {num2}, type: {type(num2)}")
+print(f"text: {text}, type: {type(text)}")
+print(f"sum_result: {sum_result}, type: {type(sum_result)}")
+print(f"sum_int: {sum_int}, type: {type(sum_int)}")
+
+# The program declares three variables: num1, num2, and text. It performs the required operations and prints the variables with their types using type().
