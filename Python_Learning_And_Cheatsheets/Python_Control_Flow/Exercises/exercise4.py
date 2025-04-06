a69d63fa6b0af4da94f2315512de57ee261638ba
Hash <dannyhash0x1@gmail.com>
2025-04-06 13:06:10 -0400

    Added exercise for user input with a secret code validation using a while loop

---
+++ Python_Learning_And_Cheatsheets/Python_Control_Flow/Exercises/exercise4.py
@@ -0,0 +1,22 @@
+"""
+Problem:
+Write a Python program that:
+
+Repeatedly asks the user to enter a secret code using a while loop.
+
+If the input is correct, print "Access Granted" and exit the loop.
+
+If the input is wrong, print "Wrong code, try again."
+"""
+
+secret_code = "Hackers123"
+
+while True:
+    user_input = input("Please enter the secret code: ")
+
+    if user_input == secret_code:
+        print("Access Granted")
+        break
+    else:
+        print("Wrong code, try again.")
+        continue
