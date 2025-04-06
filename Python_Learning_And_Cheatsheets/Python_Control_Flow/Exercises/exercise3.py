b7b94e09101fdd00b4a409f52719632953377c20
Hash <dannyhash0x1@gmail.com>
2025-04-06 13:00:15 -0400

    Added exercise for calculating the sum of numbers from 1 to N using a for loop

---
+++ Python_Learning_And_Cheatsheets/Python_Control_Flow/Exercises/exercise3.py
@@ -0,0 +1,18 @@
+"""
+Problem:
+Write a Python program that:
+
+Asks the user for a positive number N.
+
+Uses a for loop to calculate the sum of all numbers from 1 to N.
+
+Prints the result.
+"""
+
+n = int(input("Please enter a positive number: "))
+sum = 0
+
+for i in range(1, n + 1):
+    sum += i
+
+print(f"The sum of all numbers from 1 to {n} is {sum}")
