341699ef051f365ab88fd8b0e5762911c2d784db
Hash <dannyhash0x1@gmail.com>
2025-04-06 22:06:12 -0400

    Added greet_hacker function exercise with personalized greeting

---
+++ Python_Learning_And_Cheatsheets/Python_Functions_And_Parameters/Exercises/exercise1.py
@@ -0,0 +1,12 @@
+"""
+Your Task: Write a function named greet_hacker that takes one parameter—a hacker's alias—and prints a personalized greeting like:
+
+Hello, ZeroCool! Welcome to the grid.
+"""
+
+
+def greet_hacker(alias):
+    print(f"Hello, {alias}! Welcome to the grid.")
+
+
+greet_hacker("ZeroCool")
